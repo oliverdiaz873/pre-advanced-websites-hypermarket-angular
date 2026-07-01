9f4acd721e0aef6a4bca7fd5bb60ffa338bb331d
Oliver Díaz <oliverdiaz130602@gmail.com>
2026-07-01 16:02:28 -0400

    feat(layout): add barrel export for layout components

---
+++ src/app/features/layout/index.ts
@@ -0,0 +1,3 @@
+export { HeaderComponent } from './components/header/header.component';
+export { FooterComponent } from './components/footer/footer.component';
+export { LegalLayoutComponent } from './components/legal-layout/legal-layout.component';
